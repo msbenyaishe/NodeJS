fc5e8e77a6219089f66a303a6dd50e046a0c6ad4
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-20 10:02:09 +0000

    Fix

--- atelier1/rapi-front/src/components/Calculator.jsx
+++ atelier1/rapi-front/src/components/Calculator.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE = "https://node-js-uzbs.vercel.app/";
+const API_BASE = "https://node-js-uzbs.vercel.app";
 
 export default function Calculator() {
   const [n1, setN1] = useState("");
